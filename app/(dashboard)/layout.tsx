6bba251ac80d5432217f662c313b42e746a0202c
OH-JOO-YEONG <brb1111@naver.com>
2024-09-08 23:24:25 +0900

    [Feat] 사이드 메뉴, 상단바 퍼블리싱 완료

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,8 @@
+import Menu from '@/components/menu';
+import NavBar from '@/components/nav-bar';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -5,8 +10,15 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="h-screen flex">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200"></div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-slate-500">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link href="/" className="flex items-center justify-center gap-2">
+          <Image src={'/logo.png'} alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchoolCare</span>
+        </Link>
+        <Menu />
+      </div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] flex flex-col">
+        <NavBar />
         {children}
       </div>
     </div>
---
+++ components/menu.tsx
@@ -0,0 +1,29 @@
+import { menuItems } from '@/constants/menu';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((menu) => (
+        <div className="flex flex-col gap-2" key={menu.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {menu.title}
+          </span>
+          {menu.items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.label}
+              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+            >
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
